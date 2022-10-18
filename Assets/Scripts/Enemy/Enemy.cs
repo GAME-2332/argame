1ef49568c8350351364909e39c5032a9412728b8
aeonic <io@aeonic.design>
2022-10-18 12:05:17 -0500

    URP project setup

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public string name;
+    public int coinsDropped;
+    public int damage;
+    public int maxHealth;
+    public int currentHealth;
+    public float speed;
+}
