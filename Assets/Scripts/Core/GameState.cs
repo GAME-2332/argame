56d2117aa075b251d0209671e024fd5acbf6d28a
aeonic <io@aeonic.design>
2022-11-03 12:16:11 -0500

    Add GameManager and GameState, this time not a singleton

---
+++ Assets/Scripts/Core/GameState.cs
@@ -0,0 +1,13 @@
+﻿namespace XR {
+    public enum GameState {
+        Playing,
+        Paused,
+        MainMenu
+    }
+
+    static class GameStateMethods {
+        public static bool IsPlaying(this GameState state) {
+            return state == GameState.Playing;
+        }
+    }
+}
