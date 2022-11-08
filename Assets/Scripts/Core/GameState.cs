439f84d50a574bf5f8eda74ccf16c58bcb4f3f1e
aeonic <io@aeonic.design>
2022-11-08 11:37:53 -0600

    Add GameState#Shop, #IsTracking

--- Assets/Scripts/Core/GameState.cs
+++ Assets/Scripts/Core/GameState.cs
@@ -1,6 +1,9 @@
-﻿namespace XR {
+﻿using Unity.VisualScripting;
+
+namespace XR {
     public enum GameState {
         Playing,
+        Shop,
         Paused,
         MainMenu
     }
@@ -8,6 +11,11 @@
     static class GameStateMethods {
         public static bool IsPlaying(this GameState state) {
             return state == GameState.Playing;
+        }
+
+        /// Whether the game is rendering and performing XR tracking (anywhere but the main menu)
+        public static bool IsTracking(this GameState state) {
+            return state != GameState.MainMenu;
         }
     }
 }
