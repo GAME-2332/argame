65ddaf77f39f2d56bcbc5f9a713bc5acb8541e9f
aeonic <io@aeonic.design>
2022-11-14 23:01:12 -0600

    Simplify IRuntimeSerialized, add SerializeStatic attribute

---
+++ Assets/Scripts/Persistence/SerializeStaticAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Persistence {
+    /// <summary>
+    /// Add this attribute to a static instance field to serialize it. The field type must also extend <see cref="IRuntimeSerialized"/>.
+    /// </summary>
+    public class SerializeStaticAttribute : PropertyAttribute {
+        public readonly string Name;
+        
+        public SerializeStaticAttribute(string name) {
+            Name = name;
+        }
+    }
+}
