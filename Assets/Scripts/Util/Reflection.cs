24ea4089a5a5f343c1a4caea2ba3dc8d09174fc1
aeonic <io@aeonic.design>
2022-11-14 23:01:22 -0600

    Add reflection helpers, StaticField

---
+++ Assets/Scripts/Util/Reflection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+
+namespace Util {
+    public class Reflection {
+        public static IEnumerable<Type> GetTypesWithAttribute(Type attributeType) {
+            return GetTypesWithAttribute(Assembly.GetExecutingAssembly(), attributeType);
+        }
+        
+        public static IEnumerable<Type> GetTypesWithAttribute(Assembly assembly, Type attributeType) {
+            return
+                from type in assembly.GetTypes() 
+                let attributes = type.GetCustomAttributes(attributeType, true)
+                where attributes != null && attributes.Length > 0
+                select type;
+        }
+        
+        public static IEnumerable<StaticField<T>> GetStaticFieldsWithAttribute<T>(Type attributeType) {
+            return GetStaticFieldsWithAttribute<T>(Assembly.GetExecutingAssembly(), attributeType);
+        }
+
+        public static IEnumerable<StaticField<T>> GetStaticFieldsWithAttribute<T>(Assembly assembly, Type attributeType) {
+            return
+                from type in assembly.GetTypes()
+                from field in type.GetFields(BindingFlags.Static)
+                where field.FieldType == typeof(T)
+                let attributes = field.GetCustomAttributes(attributeType, true)
+                where attributes != null && attributes.Length > 0
+                select new StaticField<T>(field);
+        }
+    }
+}
---
+++ Assets/Scripts/Util/StaticField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Unity.VisualScripting;
+
+namespace Util {
+    public class StaticField<T> {
+        public FieldInfo FieldInfo { get; private set; }
+        public T Value {
+            get => Getter();
+            set => Setter(value);
+        }
+        
+        private readonly Func<T> Getter;
+        private readonly Action<T> Setter;
+
+        public StaticField(FieldInfo field) {
+            FieldInfo = field;
+
+            if (field.IsLiteral) {
+                var constant = (T) FieldInfo.GetValue(null);
+                Getter = () => constant;
+            } else {
+                Getter = () => (T) FieldInfo.GetValue(null);
+                if (FieldInfo.CanWrite()) Setter = value => FieldInfo.SetValue(null, value);
+            }
+        }
+    }
+}
