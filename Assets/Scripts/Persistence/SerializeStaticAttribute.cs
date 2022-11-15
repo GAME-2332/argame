b6b826b3f2c8d0f1bdb2a451fa322bb16342c632
aeonic <io@aeonic.design>
2022-11-15 11:19:32 -0600

    Remove name parameter from SerializeStatic attribute

--- Assets/Scripts/Persistence/SerializeStaticAttribute.cs
+++ Assets/Scripts/Persistence/SerializeStaticAttribute.cs
@@ -6,10 +6,5 @@ namespace Persistence {
     /// Add this attribute to a static instance field to serialize it. The field type must also extend <see cref="IRuntimeSerialized"/>.
     /// </summary>
     public class SerializeStaticAttribute : PropertyAttribute {
-        public readonly string Name;
-        
-        public SerializeStaticAttribute(string name) {
-            Name = name;
-        }
     }
 }
